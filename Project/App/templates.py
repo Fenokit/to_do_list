3f45e43f0f72983e2747703355f265edef898cd8
Renat Mukanov <103631832+Fenokit@users.noreply.github.com>
2023-08-27 06:31:56 +0400

    Изменил по принципу DRY

--- Project/App/templates.py
+++ Project/App/templates.py
@@ -1,13 +1,14 @@
 # Создан для работы с шаблонами – HTML, CSS, JS
+def read_html_from_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        html_content = f.read()
+    return html_content
+
 def general_html_string():
-    with open('App/Templates/index.html', 'r', encoding='utf-8') as f:
-        html_content = f.read()
-        return html_content
+    return read_html_from_file('App/Templates/index.html')
+
 def add_html_string():
-    with open('App/Templates/add.html', 'r', encoding='utf-8') as f:
-        html_content = f.read()
-        return html_content
+    return read_html_from_file('App/Templates/add.html')
+
 def delete_html_string():
-    with open('App/Templates/delete.html', 'r', encoding='utf-8') as f:
-        html_content = f.read()
-        return html_content
+    return read_html_from_file('App/Templates/delete.html')
